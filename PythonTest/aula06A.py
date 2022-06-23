9aed44d17c5a35c85ca9856dfed8d47b4bc9b318
Lorenzo Martins <105520508+LorenzoMartins7@users.noreply.github.com>
2022-06-23 13:42:06 -0300

    python

--- PythonTest/aula06A.py
+++ PythonTest/aula06A.py
@@ -1,7 +1,4 @@
-from termios import NL1
-
-
 n1 = int(input('Digite um numero'))
 n2 =  int(input('Digite outro: '))
 s = n1 + n2
-print('A soma vale', s)
+print('A soma vale entre', n1, 'e', ' vale', s)
